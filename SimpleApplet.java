93de43110497ef4ced256e10471148b958cfe7ac
sumit048 <123713221+sumit048@users.noreply.github.com>
2023-01-27 09:28:25 +0530

    Add files via upload

---
+++ SimpleApplet.java
@@ -0,0 +1,11 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+/*<applet code="SimpleApplet.class"width=300
+ height="300"</applet>
+ */
+public class SimpleApplet extends Applet{
+public void paint(Graphics g){
+	g.drawString("Welcome to Applet",150,150);
+	
+}
+}
